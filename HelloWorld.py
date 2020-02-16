da011c723122edc81d558d0f11d6d2fa3c62e30b
anthony quinn <anthonyquinn1975@outlook.com>
2020-02-16 17:27:38 +0000

    Added an input/prompt

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,14 +1,15 @@
-#Author:Anthony Quinn
-#Description: A test project.
-#Date: 16th February 2020
+# Author:Anthony Quinn
+# Description: A test project.
+# Date: 16th February 2020
 
 import datetime
+
 todays_date = datetime.datetime.now()
-print("Hello World on  the ",todays_date)
-
-
+print("Hello World on  the ", todays_date)
 
 '''This is just to check that the second committe is working'''
 
-
 '''What a day, what a stormy day'''
+
+q_request = input("What day is it today:")
+print("It's ??:", q_request)
